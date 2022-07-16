04b11a6747f1e41658d966baf7971b3f7c7b1cf7
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-07-16 22:56:04 +0600

    landing page bottom btn added

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -27,7 +27,7 @@ function LandingPage() {
                 <div className='mt-1 text-white '>
                     <h6>Give order to plant trees </h6>
                 </div>
-                
+
             </div>
             <div className='d-flex aligns-items-center justify-content-center mt-3' >
                 <div>
@@ -36,7 +36,7 @@ function LandingPage() {
                 <div className='mt-1 text-white'>
                     <h6>Get suggestions for planting trees</h6>
                 </div>
-                
+
             </div>
             <div className='d-flex aligns-items-center justify-content-center mt-3' >
                 <div>
@@ -45,7 +45,7 @@ function LandingPage() {
                 <div className='mt-1 text-white'>
                     <h6>See your planted trees realtime photo</h6>
                 </div>
-                
+
             </div>
             <div className='d-flex aligns-items-center justify-content-center mt-3' >
                 <div>
@@ -54,7 +54,7 @@ function LandingPage() {
                 <div className='mt-1 text-white'>
                     <h6>Check your area's ratio of people and tree  </h6>
                 </div>
-                
+
             </div>
             <div className='d-flex aligns-items-center justify-content-center mt-3' >
                 <div>
@@ -63,14 +63,22 @@ function LandingPage() {
                 <div className='mt-1 text-white'>
                     <h6>Check estimated oxygen production by trees </h6>
                 </div>
-                
+
             </div>
 
-            <div className='mt-5 text-center '>
+            {/* <div className='mt-5 text-center '>
                 <Button className='btn btn-success rounded-circle  shadow-lg  py-4 px-3 mt-4' onClick={() => { navigate('/Homepage') }}>Go to Home</Button>
+            </div> */}
+
+            <div>
+                <Button className='btn' style={{
+                    backgroundColor: "#1abc9c", width: "400px", height: "100px", border: 'none', borderRadius: "50% / 100%",
+                    borderBottomLeftRadius: "0", borderBottomRightRadius: "0", position: 'fixed',
+                    bottom: '0px'
+                }} onClick={() => { navigate('/Homepage') }}><h3 className='pt-3'>Get Started</h3></Button>
             </div>
-            
-        </div> 
+
+        </div>
 
 
     )
